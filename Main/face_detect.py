0117c639499ebefe86e9b0ddae06fdd0b1a94a46
SlashDK <dkhurana@andrew.cmu.edu>
2016-03-17 00:41:43 -0400

    Works with raspberry pi
    Use previous with addItems() of current one for other OS

--- Main/face_detect.py
+++ Main/face_detect.py
@@ -1,7 +1,11 @@
 import cv2
 import sys
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import datetime
 import numpy
+import copy
 
 def addItems(frame,faces,mouths,eyes):
     glasses = cv2.imread("glasses.png",-1)
@@ -13,16 +17,15 @@ def addItems(frame,faces,mouths,eyes):
     #print(joint)
     for (x, y, w, h) in faces:
         for (x2, y2, w2, h2) in eyes:
-            if(x2>x and x2+w2<x+w and y2>y and y2+h2<y+h):
+            if(x2>x and x2+w2<x+w and y2>y and y2+h2<y+h and y2+h2/2<y+h/2 and x2+w2/2>x+h/3 and x2+w2/2<x+2*h/3):
                 glasses=cv2.resize(glasses,(w2,h2))
                 for c in range(0,3):
                     frame[y2+h2/5:y2+6*h2/5, x2:x2+w2, c] =  glasses[:,:,c] * (glasses[:,:,3]/255.0) + frame[y2+h2/5:y2+6*h2/5, x2:x2+w2, c] * (1.0 - glasses[:,:,3]/255.0)
         for (x2, y2, w2, h2) in mouths:
-            if(x2>x and x2<x+w):
+            if(x2>x and x2<x+w and y2>y+h/2 and y2+h2<y+h and x2+w2/2>x+h/3 and x2+w2/2<x+2*h/3):
                 joint=cv2.resize(joint,(w2,h2))
                 for c in range(0,3):
                     frame[y2+h2/5:y2+6*h2/5, x2+w2/2:x2+3*w2/2, c] =  joint[:,:,c] * (joint[:,:,3]/255.0) + frame[y2+h2/5:y2+6*h2/5, x2+w2/2:x2+3*w2/2, c] * (1.0 - joint[:,:,3]/255.0)
-
 
     cv2.imwrite(filename,frame)
     return 
@@ -32,12 +35,17 @@ def capVideo():
     faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     mouthCascade = cv2.CascadeClassifier("Mouth.xml")
     eyesCascade = cv2.CascadeClassifier("frontalEyes35x16.xml")
-    video_capture = cv2.VideoCapture(0)
+    camera = PiCamera()
+    camera.resolution = (320, 320)
+    camera.framerate = 5
+    rawCapture = PiRGBArray(camera, size=(320, 320))
+    time.sleep(0.1)
+    #video_capture = cv2.VideoCapture(0)
 
-    while True:
+    for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         # Capture frame-by-frame
-        ret, frame = video_capture.read()
-        ret, orig = video_capture.read()
+        frame = image.array
+        orig = copy.deepcopy(frame)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         faces = faceCascade.detectMultiScale(
@@ -83,9 +91,10 @@ def capVideo():
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        rawCapture.truncate(0)
 
     # When everything is done, release the capture
-    video_capture.release()
+    camera.close()
     cv2.destroyAllWindows()
 
 capVideo()
